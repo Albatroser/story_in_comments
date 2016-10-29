ef43ec40045f0ecad63dc940e7b8fc9a2cff3ef3
Khasan Khafizov <xozzslip@gmail.com>
2016-10-29 16:48:28 +0300

    glue and story_posting modules now work

--- story_posting/models.py
+++ story_posting/models.py
@@ -7,7 +7,6 @@ class Post(models.Model):
     community = models.ForeignKey(Community)
     is_posted = models.BooleanField(default=False)
     is_proposed = models.BooleanField(default=False)
-    story_is_posted = models.BooleanField(default=False)
     vk_id_request = models.IntegerField(null=True, blank=True)
     vk_id_real = models.IntegerField(null=True, blank=True)
     vkaccount = models.ForeignKey(Vkaccount)
@@ -19,19 +18,20 @@ class Post(models.Model):
         })
 
     def post_story(self):
-        print(self.story)
         self.story.post()
 
 
 class Story(models.Model):
     name = models.CharField(max_length=300)
     parent_post = models.OneToOneField(Post, related_name='story')
+    is_posted = models.BooleanField(default=False)
 
     def post(self):
-        if not self.is_posted:
-            comments = self.comment_set.order_by("order")
-            for comment in comments:
-                comment.post()
+        comments = self.comment_set.order_by("order")
+        for comment in comments:
+            comment.post()
+        self.is_posted = True
+        self.save()
 
 
 class Comment(models.Model):
@@ -42,7 +42,7 @@ class Comment(models.Model):
 
     def post(self):
         self.vkaccount.api.method("wall.createComment", {
-            "owner_id": -self.story.post.community.vk_domen,
-            "post_id": self.story.post.vk_id,
+            "owner_id": -self.story.parent_post.community.vk_domen,
+            "post_id": self.story.parent_post.vk_id_real,
             "message": self.text,
         })
