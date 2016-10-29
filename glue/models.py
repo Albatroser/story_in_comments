6f052dd2dbab5dd788002fba903eaeaf3d818a34
Khasan Khafizov <xozzslip@gmail.com>
2016-10-29 14:41:41 +0300

    db restriction

--- glue/models.py
+++ glue/models.py
@@ -11,23 +11,3 @@ class Community(models.Model):
     vk_domen = models.IntegerField()
     name = models.CharField(max_length=300, default="")
     url = models.CharField(max_length=300, default="")
-
-
-class Post(models.Model):
-    text = models.CharField(max_length=300)
-    community = models.ForeignKey(Community)
-    is_posted = models.BooleanField(default=False)
-    vk_id = models.IntegerField(null=True, blank=True)
-    vkaccount = models.ForeignKey(Vkaccount)
-
-
-class Story(models.Model):
-    name = models.CharField(max_length=300)
-    post = models.ForeignKey(Post)
-
-
-class Comment(models.Model):
-    text = models.CharField(max_length=300)
-    vkaccount = models.ForeignKey(Vkaccount)
-    order = models.IntegerField()
-    story = models.ForeignKey(Story)
--- story_posting/models.py
+++ story_posting/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from glue.models import Community, Vkaccount
 
-# Create your models here.
+
+class Post(models.Model):
+    text = models.CharField(max_length=300)
+    community = models.ForeignKey(Community)
+    is_posted = models.BooleanField(default=False)
+    vk_id = models.IntegerField(null=True, blank=True)
+    vkaccount = models.ForeignKey(Vkaccount)
+
+
+class Story(models.Model):
+    name = models.CharField(max_length=300)
+    post = models.ForeignKey(Post)
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=300)
+    vkaccount = models.ForeignKey(Vkaccount)
+    order = models.IntegerField()
+    story = models.ForeignKey(Story)
