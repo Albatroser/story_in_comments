2fba0a80070577efebb4a1cde796135d3ee12ce8
Khasan Khafizov <xozzslip@gmail.com>
2016-10-30 00:44:53 +0300

    new adding template in glue

--- glue/urls.py
+++ glue/urls.py
@@ -5,4 +5,5 @@ from glue import views
 
 urlpatterns = [
     url(r'^communities/$', views.communities, name="communities"),
+    url(r'^adding/$', views.adding, name="adding"),
 ]
--- glue/views.py
+++ glue/views.py
@@ -8,3 +8,9 @@ def communities(request):
     template = "glue/main.html"
     communities = Community.objects.all()
     return render(request, template, {"communities": communities})
+
+
+@login_required()
+def adding(request):
+    template = "glue/adding.html"
+    return render(request, template, {})
