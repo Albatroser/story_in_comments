9c5581135b2d78b65f874e0d63444d8824e386c1
Khasan Khafizov <xozzslip@gmail.com>
2016-10-29 14:06:54 +0300

    db

--- glue/models.py
+++ glue/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Community(models.Model):
+    vk_domen = models.IntegerField()
+    name = models.CharField(max_length=300, default="")
+    url = models.CharField(max_length=300, default="")
+
+
+class Post(models.Model):
+    text = models.CharField(max_length=300)
+    community = models.ForeignKey(Community)
+    is_posted = models.BooleanField(default=False)
+    vk_id = models.IntegerField(null=True, blank=True)
+
+
+class Story(models.Model):
+    name = models.CharField(max_field=300)
+    post = models.ForeignKey(Post)
+
+
+class Comment(models.Model):
+    text = models.CharField(max_length=300)
+    order = models.IntegerField()
+    story = models.ForeignKey(Story)
