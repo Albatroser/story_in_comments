345fc06acb14124ab08974fe2e2668dad2e144ac
Khasan Khafizov <xozzslip@gmail.com>
2016-10-29 17:55:43 +0300

    community - post to foreign key relationship

--- glue/tasks.py
+++ glue/tasks.py
@@ -3,8 +3,16 @@ from story_posting.models import Post
 
 
 @app.task()
-def post_story():
+def post_things():
     for post in Post.objects.all():
+        if not post.ready_for_posting:
+            print("post is not ready %s" % post.text)
+            return
+
         if post.is_posted and post.vk_id_real and not post.story.is_posted:
-            print(post.text)
+            print("posting story to community %s" % post.text)
             post.post_story()
+
+        if not post.is_proposed:
+            print("posting post to community %s" % post.text)
+            post.post()
